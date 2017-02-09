da2ecf140a749050752ccdc4d800eb26d7104758
andaistvan <andaistvan@gmail.com>
2017-02-09 09:19:52 +0100

    RENAME PLUGIN
    nagy állítgatás előtt

--- contact/components/ContactForm.php
+++ contact/components/ContactForm.php
@@ -1,13 +1,36 @@
 <?php namespace Arteriaweb\Contact\Components;
 
 use Cms\Classes\ComponentBase;
+use Input;
+use Mail;
 
 class ContactForm extends ComponentBase
 {
 	public function componentDetails(){
 		return [
-			'name' => 'ContactForm',
+			'name' => 'contactform',
 			'description' => 'Contact form plugin',
 		];
+	}
+
+	public function onMailSent(){
+
+		$vars = [
+			'name' => Input::get('vezeteknev'),
+			'vezeteknev' => Input::get('vezeteknev'),
+			'keresztnev' => Input::get('keresztnev'),
+			'email' => Input::get('email'),
+			'telefon' => Input::get('telefon'),
+			'erkezes' => Input::get('erkezes'),
+			'message' => Input::get('message'),
+		];
+
+		Mail::send('arteriaweb.contact::mail.message', $vars, function($message) {
+
+		    $message->to('andaistvan@gmail.com', 'Admin Person');
+		    $message->subject('New contact form plugin message!');
+
+		});
+
 	}
 }
