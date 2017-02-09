30960f3df0a4be5cefff8ca84eaf7224fb2702ee
andaistvan <andaistvan@gmail.com>
2017-02-09 06:10:44 +0100

    MODAL - before make

--- contact/Plugin.php
+++ contact/Plugin.php
@@ -6,6 +6,9 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
+		 return[
+			'Arteriaweb\Contact\Components\ContactForm' => 'contactform', 
+		 ];
     }
 
     public function registerSettings()
---
+++ contact/components/ContactForm.php
@@ -0,0 +1,13 @@
+<?php namespace Arteriaweb\Contact\Components;
+
+use Cms\Classes\ComponentBase;
+
+class ContactForm extends ComponentBase
+{
+	public function componentDetails(){
+		return [
+			'name' => 'ContactForm',
+			'description' => 'Contact form plugin',
+		];
+	}
+}
