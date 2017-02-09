f4f5aa66cff24d7dac5f3397eeda126254531058
andaistvan <andaistvan@gmail.com>
2017-02-09 05:51:36 +0100

    1. database done

---
+++ contact/updates/builder_table_create_arteriaweb_contact_.php
@@ -0,0 +1,27 @@
+<?php namespace Arteriaweb\Contact\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateArteriawebContact extends Migration
+{
+    public function up()
+    {
+        Schema::create('arteriaweb_contact_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('vezeteknev')->nullable();
+            $table->text('keresztnev')->nullable();
+            $table->string('telefon')->nullable();
+            $table->text('email')->nullable();
+            $table->text('indulas');
+            $table->text('erkezes');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('arteriaweb_contact_');
+    }
+}
